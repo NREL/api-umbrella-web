da21e905a8faf760b87576ba840b23162d310dea
Nick Muerdter <nick.muerdter@nrel.gov>
2013-05-01 23:04:46 -0600

    Force deployment of the gsa branch.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,17 +1,6 @@
-# Force a branched deployment on the production server for the GSA demo.
-ENV["BRANCH"] = "gsa"
-require "capistrano_nrel_ext/recipes/branches"
-
 # Set the servers for this stage.
-role :app, "devprod-int.nrel.gov"
-role :web, "devprod-int.nrel.gov"
-
-# Define the primary db server as our app server so database migrations can run
-# from the code checkout there.
-role :db, "devprod-int.nrel.gov", :primary => true
-
-# On our real database server, don't actually perform a code deployment.
-role :db, "devprod-int-db.nrel.gov", :no_release => true
+role :app, "web1-api.hopto.org"
+role :web, "web1-api.hopto.org"
 
 # Set the base path for deployment.
 set :deploy_to_base, "/srv/data/devprod-int"
@@ -24,3 +13,6 @@ set :copy_exclude, [".git"]
 
 # Set the Rails environment.
 set :rails_env, "production"
+
+set :user, "root"
+ssh_options[:keys] = ["/vagrant/workspace/aws_nmuerdter.pem"]
