e04843ddff27ce153cc54d67fcfb71146e93ed93
Nick Muerdter <nick.muerdter@nrel.gov>
2013-08-04 10:44:33 -0600

    Revamp admin login process to allow other login mechanisms.
    Since not everyone that needs access to api.data.gov has MAX.gov, now
    supporting MAX.gov, MyUSA, Google, and Persona.

--- app/controllers/admin_sessions_controller.rb
+++
@@ -1,6 +0,0 @@
-class AdminSessionsController < Devise::SessionsController
-  def new
-    provider = if(Rails.env.development?) then "developer" else "cas" end
-    redirect_to(admin_omniauth_authorize_path(:provider => provider))
-  end
-end
